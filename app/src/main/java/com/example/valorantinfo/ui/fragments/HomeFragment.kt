648172ff1371918940b5315efd73ebb81d2d63de
amanjn38 <amanjn38@gmail.com>
2025-03-30 11:49:55 +0530

    Added Level Border implementation

--- app/src/main/java/com/example/valorantinfo/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/valorantinfo/ui/fragments/HomeFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.navigateUp
 import com.example.valorantinfo.R
 import com.example.valorantinfo.data.models.Category
 import com.example.valorantinfo.databinding.FragmentHomeBinding
--- app/src/main/java/com/example/valorantinfo/ui/fragments/LevelBorderDetailsFragment.kt
+++ app/src/main/java/com/example/valorantinfo/ui/fragments/LevelBorderDetailsFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.example.valorantinfo.data.models.levelborder.LevelBorder
